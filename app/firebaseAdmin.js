c7986e47d3502da270185603ade45c09aa3f130d
Mehran Beikahmadi <mehranbeikahmadi@gmail.com>
2025-02-12 18:22:31 -0800

    Updated Firebase Admin SDK configuration

--- app/firebaseAdmin.js
+++ app/firebaseAdmin.js
@@ -1,19 +1,11 @@
-import * as admin from 'firebase-admin';
-
-//import serviceAccount from '../penpalmagicapp-firebase-adminsdk-czr2x-7f1623a3ac.json';
-
-const serviceAccount = {
-  projectId: "penpalmagicapp",
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle line breaks in private key
-};
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
-export const auth = admin.auth();
-export const db = admin.firestore();
-export const storage = admin.storage();
+export default admin;
